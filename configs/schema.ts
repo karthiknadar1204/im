128f654210aede31ac9633087789a65b14d6270b
karthik <karthiknadar1204@gmail.com>
2025-07-11 10:46:56 +0530

    clerk setup done

---
+++ configs/schema.ts
@@ -0,0 +1,26 @@
+import {
+    pgTable,
+    serial,
+    text,
+    date,
+    integer,
+    numeric,
+    varchar,
+    boolean,
+    timestamp,
+    jsonb,
+    primaryKey,
+    uniqueIndex,
+  } from 'drizzle-orm/pg-core';
+  import { relations } from 'drizzle-orm';
+
+export const users=pgTable('users',{
+    id: serial('id').primaryKey(),
+    name: text('name').notNull(),
+    email: text('email').notNull().unique(),
+    password: text('password').notNull(),
+    image: text('image').notNull(),
+    createdAt: timestamp('created_at').notNull().defaultNow(),
+    updatedAt: timestamp('updated_at').notNull().defaultNow(),
+})
+
---
+++ drizzle.config.ts
@@ -0,0 +1,10 @@
+export default {
+    schema: './configs/schema.ts',
+    dialect: 'postgresql',
+    dbCredentials: {
+      url: process.env.DATABASE_URL,
+      // ssl: {
+      //   rejectUnauthorized: false,
+      // },
+    },
+  };
